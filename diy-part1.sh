702dbbddbf1efc542d7116401c11f322ccd1fa0f
gdstchdr2 <73393727+gdstchdr2@users.noreply.github.com>
2020-10-29 10:02:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,10 +21,10 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 # git clone https://github.com/firkerword/v2fly.git package/v2fly
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 svn co https://github.com/firker/diy-ziyong/trunk/smartdns package/smartdns
 svn co https://github.com/firker/diy-ziyong/trunk/vssr/https-dns-proxy package/https-dns-proxy
-svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lienol
 # git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash && cd package/OpenClash && git checkout 04bbcf6f56c794d8ee1013123ed2d0dd2a89cb55
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/brook
@@ -35,3 +35,4 @@ svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lienol
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package/naiveproxy package/naiveproxy
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray package/v2ray
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray-plugin package/v2ray-plugin
+git clone https://github.com/siropboy/mypackages/tree/main/luci-app-control-weburl.git package/luci-app-control-weburl
